b6fc510d527785d56c296f2dc5885c51bab0b5ff
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2016-08-05 17:46:24 +0200

    Initial commit on GitHub

---
+++ src/css2polymerStyleModule.js
@@ -0,0 +1,34 @@
+var  fs = require('fs-extra');
+
+if (process.argv.length < 4) {
+  console.log("Use: node css2PolymerStyleModule.js CSS_SOURCE_FILE TARGET_FOLDER\n");
+  process.exit();  
+}
+
+var cssFile = process.argv[2];
+
+var targetFolder = process.argv[3];
+
+var elementName = function(filename) {
+  return filename.replace('.css', '').replace('.min','-min');
+}
+
+var getHeader = function(filename) {
+  
+  return `<dom-module is="${elementName(filename)}"><template><style>\n`;
+}
+var getFooter = function() {
+  return `\n</style></template></dom-module>`;
+}
+
+fs.ensureDirSync(targetFolder);
+ 
+var splittedPath = cssFile.split('/');
+var filename = splittedPath[splittedPath.length-1];
+ 
+var data = fs.readFileSync(cssFile, "utf8"); 
+var out = getHeader(filename) + data + getFooter();
+
+fs.writeFileSync(`${targetFolder}/${elementName(filename)}.html`, out);
+
+  
